833ff8d9da48826bd876ae7a5284bce760bff8aa
3x-haust <lyusungyun@icloud.com>
2025-11-24 17:14:46 +0900

    fix: update user endpoint path to plural form

--- src/main/java/io/github/_3xhaust/root_server/domain/user/controller/UserController.java
+++ src/main/java/io/github/_3xhaust/root_server/domain/user/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
