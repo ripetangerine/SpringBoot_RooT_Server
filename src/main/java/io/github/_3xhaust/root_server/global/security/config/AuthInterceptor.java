363f104fdb7e925e5e86571fc19f847faee8d12e
ripetangerine <dev.ripetangerine@gmail.com>
2026-01-29 15:13:19 +1100

    Add: Hilt depandency (Grandle) scrimage-core, scrimage-webp

--- src/main/java/io/github/_3xhaust/root_server/global/security/config/AuthInterceptor.java
+++ src/main/java/io/github/_3xhaust/root_server/global/security/config/AuthInterceptor.java
@@ -1,24 +1,35 @@
-package io.github._3xhaust.root_server.global.security.config;
-
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.Map;
-
-// TODO : product 환경에서 보안 관련 인증 권한 부여
-public  class  AuthInterceptor  implements  HandshakeInterceptor {
-
-    @Override
-    public  boolean  beforeHandshake (ServerHttpRequest request, ServerHttpResponse response,
-                                      WebSocketHandler wsHandler, Map<String, Object> attributes) {
-        String  token  = extractToken(request);
-        return isValidToken(token);
-    }
-
-    @Override
-    public  void  afterHandshake (ServerHttpRequest request, ServerHttpResponse response,
-                                  WebSocketHandler wsHandler, Exception exception) {
-    }
-}
+//package io.github._3xhaust.root_server.global.security.config;
+//
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+//import org.springframework.web.socket.WebSocketHandler;
+//import org.springframework.web.socket.server.HandshakeInterceptor;
+//
+//import java.util.Map;
+//
+//// TODO : product 환경에서 보안 관련 인증 권한 부여
+//public class AuthInterceptor implements HandshakeInterceptor{
+//
+//    @Override
+//    public boolean beforeHandshake(
+//            ServerHttpRequest request,
+//            ServerHttpResponse response,
+//            WebSocketHandler wsHandler,
+//            Map<String, Object> attributes
+//    ){
+////        String token = extract Token(request);
+////        return isValidToken(token);
+//        // 컴파일 디버그
+//        return false;
+//    }
+//
+//    @Override
+//    public  void  afterHandshake (
+//            ServerHttpRequest request,
+//            ServerHttpResponse response,
+//            WebSocketHandler wsHandler,
+//            Exception exception
+//    ) {
+//        // TODO : logic
+//    }
+//}
